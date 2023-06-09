9ad9d8d3a0df6c8761c84eb6cb7a23c982945f10
edo92 <edoter92@gmail.com>
2023-06-09 00:48:02 -0700

    preloader setup

--- apps/preload/src/main.ts
+++ apps/preload/src/main.ts
@@ -1,1 +1,9 @@
-console.log('Hello World');
+import { ipcRenderer, contextBridge } from 'electron';
+
+const API = {
+  greet: (message: unknown) => ipcRenderer.invoke('greet', message),
+};
+
+contextBridge.exposeInMainWorld('api', API);
+
+export type API = typeof API;
